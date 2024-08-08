cffde85ded44091cc92f2f274555082a7f706e95
João Sambongo <168141893+joaosambongo@users.noreply.github.com>
2024-08-08 19:45:04 +0100

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Huburguer_Ecommerce.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
